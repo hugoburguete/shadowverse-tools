51cdc7161b365c130269ddc13f89fbb7d250858e
Hugo Burguete <hugoburguete@hotmail.com>
2024-05-18 11:32:09 +0100

    #15 Centralize images and apply lazy-loading

--- src/components/CardDisplay/CardDisplay.tsx
+++ src/components/CardDisplay/CardDisplay.tsx
@@ -1,18 +1,10 @@
 import { CardSimplified } from '../../entities/card';
+import Image from '../Image';
 
 export type CardProps = {
   card: CardSimplified;
 };
 
 export const CardDisplay = ({ card: { image, name } }: CardProps) => {
-  return (
-    <div className="h-0 pb-[140%] relative w-full">
-      <img
-        loading="lazy"
-        className="w-full h-full block absolute"
-        src={image}
-        alt={name}
-      />
-    </div>
-  );
+  return <Image src={image} alt={name} aspectRatio="10/14" />;
 };
--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,10 +1,23 @@
 export type ImageProps = React.DetailedHTMLProps<
   React.ImgHTMLAttributes<HTMLImageElement>,
   HTMLImageElement
->;
+> & {
+  aspectRatio: string;
+};
 
-const Image = ({ src, alt, ...rest }: ImageProps) => {
-  return <img src={src} alt={alt} {...rest} />;
+const Image = ({ src, alt, aspectRatio, className, ...rest }: ImageProps) => {
+  return (
+    <img
+      loading="lazy"
+      src={src}
+      alt={alt}
+      style={{
+        aspectRatio,
+      }}
+      className={`w-full object-contain ${className}`}
+      {...rest}
+    />
+  );
 };
 
 export default Image;
